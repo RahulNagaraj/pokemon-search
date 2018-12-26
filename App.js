950ae2fa37aaa3001e8e75f1a8913e870c7c978e
Rahul N <rahul.nagarajmv@gmail.com>
2018-12-26 19:30:58 +0530

    create a landing page for app

--- App.js
+++ App.js
@@ -1,11 +1,31 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Platform,
+  ImageBackground,
+} from "react-native";
+import { Button } from "native-base";
+
+const backgroundImage = require("./assets/landing.jpg");
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <ImageBackground source={backgroundImage} style={styles.imageStyle}>
+          <View style={styles.viewStyle}>
+            <Text style={styles.titleStyle}>Welcome to PokeSearch</Text>
+            <Button
+              style={styles.buttonStyle}
+              block={true}
+              onPress={() => alert("started")}
+            >
+              <Text style={styles.buttonText}>Start Searching...</Text>
+            </Button>
+          </View>
+        </ImageBackground>
       </View>
     );
   }
@@ -14,8 +34,27 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: Platform.OS === "android" ? 24 : 16,
+  },
+  imageStyle: {
+    width: "100%",
+    height: "100%",
+  },
+  viewStyle: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  titleStyle: {
+    fontSize: 32,
+    color: "#2c387e",
+    textAlign: "center",
+  },
+  buttonStyle: {
+    margin: 10,
+  },
+  buttonText: {
+    color: "white",
   },
 });
