7820eab15e35cd14416a0435beda4ee12a1ddbfc
Rahul N <rahul.nagarajmv@gmail.com>
2018-12-26 22:49:11 +0530

    display header

--- src/PokeList.js
+++ src/PokeList.js
@@ -1,18 +1,42 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, ScrollView, View } from "react-native";
 
 class PokeList extends Component {
   render() {
     const { pokeDetails } = this.props;
+    if (!pokeDetails.id) {
+      return (
+        <View>
+          <Text style={styles.noPokemonStyle}>
+            Enter a pokemon to get the details.
+          </Text>
+        </View>
+      );
+    }
     return (
-      <View>
-        <Text>List of Pokemons</Text>
-        <Text>{JSON.stringify(pokeDetails)}</Text>
+      <View style={styles.pokeListContainer}>
+        <Text style={styles.pokeListHeader}>
+          #{pokeDetails.id} - {pokeDetails.name.toUpperCase()}
+        </Text>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  pokeListContainer: {
+    flex: 1,
+  },
+  noPokemonStyle: {
+    textAlign: "center",
+    fontSize: 24,
+    color: "#ff1744",
+  },
+  pokeListHeader: {
+    fontSize: 30,
+    textAlign: "center",
+    color: "#f50057",
+  },
+});
 
 export default PokeList;
