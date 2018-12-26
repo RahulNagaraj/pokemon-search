96b0064b334cd6d6a35994b76e5489a159ce1ce6
Rahul N <rahul.nagarajmv@gmail.com>
2018-12-26 20:34:45 +0530

    header section of search component

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ import Search from "./src/Search";
 
 export default class App extends React.Component {
   state = {
-    currentScreen: "landing",
+    currentScreen: "search",
   };
   switchScreen = currentScreen => {
     this.setState({ currentScreen });
--- src/Search.js
+++ src/Search.js
@@ -1,16 +1,49 @@
 import React, { Component } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import {
+  Header,
+  Input,
+  Icon,
+  Button,
+  Item,
+  Left,
+  Body,
+  Title,
+} from "native-base";
 
 class Search extends Component {
+  state = {
+    pokeText: "",
+  };
+  setPokemonText = val => {
+    this.setState({
+      pokeText: val,
+    });
+  };
+  searchPoke = () => {};
   render() {
+    const { pokeText } = this.state;
     return (
       <View>
-        <Text>Search</Text>
+        <Header searchBar rounded>
+          <Item>
+            <Icon name="ios-search" onPress={this.searchPoke} />
+            <Input
+              value={pokeText}
+              placeholder="Search Pokemon"
+              onChangeText={this.setPokemonText}
+            />
+          </Item>
+        </Header>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  searchContainer: {
+    flex: 1,
+  },
+});
 
 export default Search;
